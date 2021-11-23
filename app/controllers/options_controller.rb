9d2de5e6df8b0415633956714a19861092f986f6
Melanie Couronne <melanie.couronne@gmail.com>
2021-11-23 17:57:57 +0100

    Add CRUD to option

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,29 @@
 class OptionsController < ApplicationController
+
+  def new
+    @option = Option.new
+  end
+
+  def create
+    @interaction = Interaction.find(paralms[:interaction_id])
+    @option = Option.find(option_params)
+    if @option.save
+      redirect_to topic_path(@interaction.topic)
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @option = Option.find(params[:i])
+    @option.destroy
+
+    redirect_to topic_path(@interaction.topic)
+  end
+
+  private
+
+  def option_params
+    params.require(:option).permit(:title, :next_accroche, :position)
+  end
 end
