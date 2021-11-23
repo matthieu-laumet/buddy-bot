82d5e86eab4077c407215fd14df9d5d1439e5a1c
Melanie Couronne <melanie.couronne@gmail.com>
2021-11-23 12:01:50 +0100

    Created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Topic.destroy_all
+Interaction.destroy_all
+Option.destroy_all
+
+puts 'Creating 1 fake topic...'
+
+puts 'Creating 1 topic...'
+  topic = Topic.new(
+    title: "Onionland"
+    first_accroche: "Ca vous interesse d'en savoir plus?"
+    description: "Au programme, tout un univers à porté d'onion!"
+  )
+  topic.save!
+  puts "Created #{topic.title}"
+
+puts 'Creating 1 interaction...'
+  interaction = Interaction.new(
+    content: "interaction - content test"
+    question: "On continue?"
+    html_content: "www.lewagon.fr"
+    topic_id: 1
+    position: 1
+  )
+  interaction.save!
+  puts "Created #{interaction.content}"
+
+puts 'Creating 1 option...'
+  option = Option.new(
+    title: "100% d'accord!"
+    next_accroche: "next accroche pour continuer"
+    interaction_id: 1
+    position: 1
+  )
+  option.save!
+  puts "Created #{option.title}"
+
+
+puts 'Finished!'
