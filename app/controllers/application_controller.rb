a8c97a2eb30c0f481e4b38b2e030ffc9ebedbbdf
Christopher Marchand <chris.marchand.pro@gmail.com>
2021-11-25 14:51:15 +0100

    bcp de choses faites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
+  before_action :configure_account_update_params, only: [:update], if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
 
 
   def configure_sign_up_params
