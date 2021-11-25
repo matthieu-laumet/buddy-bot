c8a9ace36ff73b872ba9bda1bb325caeaa4317ca
Melanie Couronne <melanie.couronne@gmail.com>
2021-11-25 18:06:35 +0100

    Fixed js

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -7,7 +7,7 @@ class InteractionsController < ApplicationController
     if @interaction.save
       redirect_to topic_path(@interaction.topic)
     else
-      render topic_path(@interaction.topic)
+      redirect_to topic_path(@interaction.topic)
     end
   end
 
