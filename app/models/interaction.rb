71386b1cf660f587c58792ecba466130434897f9
Melanie Couronne <melanie.couronne@gmail.com>
2021-11-26 12:17:15 +0100

    Fixed interaction's destroy action with dependent post table

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,7 +1,8 @@
 class Interaction < ApplicationRecord
   belongs_to :topic
   has_many :options, dependent: :destroy
+  has_many :posts, dependent: :destroy
   has_one_attached :photo
 
-  validates :position, presence: true, numericality: true, uniqueness: true
+  validates :position, presence: true, numericality: true, uniqueness: { scope: :topic }
 end
